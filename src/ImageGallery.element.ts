1df0c3db257f5abac8bd459fcc1cc9345627f5e8
Henry Bergström <henrybergstrom@protonmail.com>
2021-05-14 09:36:21 +0200

    typescript and webpack cinfigured

---
+++ src/ImageGallery.element.ts
@@ -0,0 +1,8 @@
+import { LitElement, html, customElement } from 'lit-element';
+
+@customElement('image-gallery')
+export class ImageGallery extends LitElement {
+  render () {
+    return html`<div> Hello from ImageGallery</div>`;
+  }
+}
